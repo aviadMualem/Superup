68131ef48f21db5ae1ba10c20e952b76f12ffdf8
Aviad <aviad67@hotmail.com>
2019-07-01 21:47:33 +0300

    Aviad first commit

---
+++ gs-rest-service-master/complete/src/main/java/hello/InputProcessor.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class InputProcessor {
+
+    public InputProcessor() {
+    }
+    public String StringToWeb(String name_string) {
+        return "Hello " + name_string;
+    }
+}
