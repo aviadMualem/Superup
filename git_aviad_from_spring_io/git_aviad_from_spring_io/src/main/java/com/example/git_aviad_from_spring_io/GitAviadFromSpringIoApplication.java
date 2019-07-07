a850501903c51ff48bf852cd203dca517e74d608
Aviad <aviad67@hotmail.com>
2019-07-07 16:09:02 +0300

    for moving branch

---
+++ git_aviad_from_spring_io/git_aviad_from_spring_io/src/main/java/com/example/git_aviad_from_spring_io/GitAviadFromSpringIoApplication.java
@@ -0,0 +1,13 @@
+package com.example.git_aviad_from_spring_io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitAviadFromSpringIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitAviadFromSpringIoApplication.class, args);
+	}
+
+}
