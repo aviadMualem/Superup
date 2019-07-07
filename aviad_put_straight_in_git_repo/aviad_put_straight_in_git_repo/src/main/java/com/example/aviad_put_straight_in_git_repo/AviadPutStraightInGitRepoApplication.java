2b35f6744aae0658e3751431e148e286a5f90ef9
Aviad <aviad67@hotmail.com>
2019-07-07 15:54:39 +0300

    aviad first project git works

---
+++ aviad_put_straight_in_git_repo/aviad_put_straight_in_git_repo/src/main/java/com/example/aviad_put_straight_in_git_repo/AviadPutStraightInGitRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.aviad_put_straight_in_git_repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviadPutStraightInGitRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AviadPutStraightInGitRepoApplication.class, args);
+	}
+
+}
