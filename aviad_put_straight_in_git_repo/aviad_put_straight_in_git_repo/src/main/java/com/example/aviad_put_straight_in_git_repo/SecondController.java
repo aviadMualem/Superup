c9c9c91e14825a8841376bb023b71508ab092c01
Aviad <aviad67@hotmail.com>
2019-07-07 23:24:14 +0300

    including eclipse

--- aviad_put_straight_in_git_repo/aviad_put_straight_in_git_repo/src/main/java/com/example/aviad_put_straight_in_git_repo/SecondController.java
+++ aviad_put_straight_in_git_repo/aviad_put_straight_in_git_repo/src/main/java/com/example/aviad_put_straight_in_git_repo/SecondController.java
@@ -1,17 +1,31 @@
 package com.example.aviad_put_straight_in_git_repo;
 
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
 
-
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+//@RestController
+@Controller
+@ResponseBody
 public class SecondController {
     @RequestMapping("/api/hello_second/{name_of_person}")
     public String main_func(@PathVariable(value="name_of_person")  String name) {
-        return "Hello " + name;
+
+        if(name=="Tomer")
+        {
+            return "<html>" +
+                    "<header><title>This is title</title></header>" +
+                    "<body>" +
+                    "Hello TOMER" +
+                    "</body>" +
+                    "</html>" ;
+        }
+        return "<html>" +
+                "<header><title>This is title</title></header>" +
+                "<body>" +
+                "Hello world madafaka" +
+                "</body>" +
+                "</html>" ;
     }
 
 }
